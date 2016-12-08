bbfdbe837f2a57f673473caed87382259115e4bf
PauloSergioRomaoJunior <PauloSergioRomaoJunior@users.noreply.github.com>
2016-12-08 17:53:49 -0200

    Create init.js

---
+++ SpeedCache/assets/js/init.js
@@ -0,0 +1,3 @@
+<script>
+!function(a){function f(a){var b=.016*a,c=parseInt(b);return c}void 0==typeof temp&&a.preventDefault();for(var b=60*temp+61,c=60,d=b,e=0;e<b;++e)!function(a){setTimeout(function(){console.clear(),d-=1,0!=c?c-=1:c=60,console.log("Falta "+f(d)+":"+c+" Pra Renovar o Cache"),a==b-1&&location.reload()},1e3*e)}(e)}();	
+</script>
